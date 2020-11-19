7bd4f8f71bb9bdacdadecef1139a874710b2a84a
Glitch (hello-express) <none>
2020-11-19 10:54:24 +0000

    👕🍋 Checkpoint ./events/guildMemberAdd.js:14391105/217 ./LONA.js:14391105/29 ./komutlar/yasakliTag.js:14391105/642

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,8 +1,10 @@
-module.exports.event = (uye, client = global.client, cfg = require("../config.json"), db = require("quick.db")) => {
+module.exports.event = async (uye, client = global.client, cfg = require("../config.json"), db = require("quick.db")) => {
+  
   let yasakliTag = db.get(`yasakliTag_${uye.guild.id}`) || [];
+  let yasakliTagRol = db.get(`yasakliTagRol_${uye.guild.id}`) || "";
   
   if (yasakliTag.includes(uye.user.username.split(""))) {
-   
+   await uye.roles.add(cfg.roles.yasakliTagRol).catch();
   };
 };
 
---
+++ komutlar/yasakliTag.js
@@ -0,0 +1,12 @@
+module.exports.execute = ({author, cfg, msg, args}, db = require("quick.db")) => {
+  
+};
+
+module.exports.help = {
+  name: "yasaklıTag",
+  alias: [
+    "yasakliTag",
+    "yasaklıTagAyarla",
+    "yasakliTagAyarla"
+  ]
+};
