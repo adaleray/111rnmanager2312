457ec936fb7a5e4b1b6a3d6a4e45af546ddc4240
Glitch (hello-express) <none>
2020-11-19 15:20:49 +0000

    🍽🌅 Checkpoint ./komutlar/yasaklıTag.js:14391105/1709

--- komutlar/yasaklıTag.js
+++ komutlar/yasaklıTag.js
@@ -1,10 +1,13 @@
-module.exports.execute = ({client, msg, author, args, db, cfg}) => {
+module.exports.execute = async ({client, msg, author, args, db, cfg}) => {
   if (!cfg.sahipler.includes(author.id)) return;
   
-  let type = args[0];
-  let rol = msg.mentions.roles.first() || msg.guild.roles.cache.get(args[1]);
-  let taglar = args.slice(2).join("").split("");
-  msg.channel.send(taglar);
+  const yasakliTag = db.get(`yasakliTag_${msg.guild.id}`) || {};
+  
+  const rol = msg.mentions.roles.first() || msg.guild.roles.cache.get(args[0]);
+  const taglar = args.slice(1).join("").split("");
+  
+  
+  
 };
 
 module.exports.help = {
