43536ebe5f648d8180815d7df595d67e60ea6656
Glitch (hello-express) <none>
2020-11-19 12:15:44 +0000

    🎖💈 Checkpoint ./events/guildMemberAdd.js:14391105/3538 ./config.json:14391105/60

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,29 +1,5 @@
 module.exports.event = async (uye, client = global.client, cfg = require("../config.json"), db = require("quick.db")) => {
-  
-  let yasakliTag = db.get(`yasakliTag_${uye.guild.id}`) || [];
-  let yasakliTagRol = db.get(`yasakliTagRol_${uye.guild.id}`) || "";
-  let fakeRol = db.get(`fakeRol_${uye.guild.id}`) || "";
-  let zaman = (new Date().getTime() - uye.user.createdAt.getTime());
-
-  if (yasakliTag.includes(uye.user.username.split(""))) {
-    if (yasakliTagRol === "") {
-      await uye.roles.add(cfg.roles.unregister); 
-      await uye.guild.channels.cache.get(cfg.chats.kayıtChat)
-      .send(
-      `${uye} adlı üye yasaklı bir tag kullanıyor fakat yasaklı tag rolü girilmediği için kayıtsız permi verdim.`
-    );
-  } else {
-      await uye.roles.add(cfg.roles.yasakliTagRol).catch();
-      await uye.guild.channels.cache.get(cfg.chats.kayıtChat).send({ embed: { description: `${uye} adlı üye isminde yasaklı tag bulundurduğu için yasaklı taga atıldı.`}});
-      if (uye.roles.cache.get(cfg.roles.unregister)) uye.roles.remove(cfg.roles.unregister).catch(err => console.log(err.message));
-    };
-  };
-  
-  if (zaman < client.getDate(1, "hafta")) {
-    if (fakeRol === "") return;
-  } else {
-    
-  };
-};
+  console.log("piç girdi");
+ };
 
 module.exports.help = { name: "guildMemberAdd" };
