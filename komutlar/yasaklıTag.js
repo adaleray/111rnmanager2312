094058325e337636a91bfcc5c9f7ef2e388c5eba
Glitch (hello-express) <none>
2020-11-19 15:55:09 +0000

    📙🕋 Checkpoint ./komutlar/yasaklıTag.js:14391105/1524

--- komutlar/yasaklıTag.js
+++ komutlar/yasaklıTag.js
@@ -1,16 +1,12 @@
 module.exports.execute = async ({client, msg, author, args, db, cfg}) => {
   if (!cfg.sahipler.includes(author.id)) return;
-  
   var evet = "✅";
   var hayir = "❌";
-  
   let type = args[0].toLowerCase();
-  
   if (type === "rol-ekle") {
     const rol = msg.mentions.roles.first() || msg.guild.roles.cache.get(args[1]);
     function onlarFilterBenBeko(r, u) { return [evet, hayir].includes(r.emoji.name) && u.id === msg.author.id };
     if (!rol) return msg.channel.send("**Bir rol etiketlemeli veya idsini girmelisin.**").then(m => m.delete({ timeout: 5000 }));
-    
     if (db.get(`yasakliTagRol_${msg.guild.id}`)) {
       msg.channel.send({
         embed: { 
@@ -21,10 +17,35 @@ module.exports.execute = async ({client, msg, author, args, db, cfg}) => {
       }).then(async (m) => {
         await m.react(evet);
         await m.react(hayir);
-        
+        m.awaitReactions(onlarFilterBenBeko, {
+          max: 1
+        }).then(async (collected) => {
+          let cvp = collected.first();
+          if (cvp.emoji.name === evet) {
+            await db.set(`yasakliTagRol_${msg.guild.id}`, rol.id);
+            await m.delete();
+            await msg.channel.send({
+              embed: {
+                description: `Başarıyla yeni yasaklı tag rolünü ${rol} olarak ayarladım.`,
+                timestamp: new Date(),
+                color: Math.floor(Math.random() * (0xFFFFFF + 1))
+              }
+            }).then(msj => msj.delete({ timeout: 5000 }));
+          } else {
+             m.edit("Komut iptal edildi.").then(m => m.delete({ timeout: 4000 }));
+          };
+        });
       });
-    }
-    
+    } else {
+      await db.set(`yasakliTagRol_${msg.guild.id}`, rol.id);
+      msg.channel.send({
+        embed: { 
+          description: `Başarıyla yeni yasaklı tag rolünü ${rol} olarak ayarladım.`,
+          timestamp: new Date(),
+          color:Math.floor(Math.random() * (0xFFFFFF + 1))
+        }
+      });
+    };
   } else if (type === "rol-sil") {
     
   } else if (type === "tag-ekle") {
