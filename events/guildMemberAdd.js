8cf50b9bbce341155f5cac604217f8a69b491fec
Glitch (hello-express) <none>
2020-11-19 12:27:26 +0000

    🌺🍖 Checkpoint ./events/guildMemberAdd.js:14391105/2330 ./LONA.js:14391105/11824 ./config.json:14391105/155

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,47 @@
 module.exports.event = async (uye, client = global.client, cfg = require("../config.json"), db = require("quick.db")) => {
-  console.log("piç girdi");
- };
+  
+  let yasakliTag = db.get(`yasakliTag_${uye.guild.id}`) || [];
+  let yasakliTagRol = db.get(`yasakliTagRol_${uye.guild.id}`) || "";
+  let fakeRol = db.get(`fakeRol_${uye.guild.id}`) || "";
+  let isimYas = db.get(`isimYas_${uye.guild.id}`) || "kapalı";
+  
+  let zaman = (new Date().getTime() - uye.user.createdAt.getTime());
+
+  if (yasakliTag.includes(uye.user.username.split(""))) {
+    if (yasakliTagRol === "") {
+      await uye.roles.add(cfg.roles.unregister).catch();
+      await uye.guild.channels.cache.get(cfg.chats.kayıtChat)
+      .send(
+      `${uye} adlı üye yasaklı bir tag kullanıyor fakat yasaklı tag rolü girilmediği için kayıtsız permi verdim.`
+    ).catch();
+  } else {
+      await uye.roles.add(cfg.roles.yasakliTagRol).catch();
+      await uye.guild.channels.cache.get(cfg.chats.kayıtChat).send({ embed: { description: `${uye} adlı üye isminde yasaklı tag bulundurduğu için yasaklı taga atıldı.`}}).catch();
+      if (uye.roles.cache.get(cfg.roles.unregister)) uye.roles.remove(cfg.roles.unregister).catch(err => console.log(err.message));
+    };
+  };
+  
+  if (zaman < client.getDate(1, "hafta")) {
+    if (fakeRol === "") {
+     await uye.roles.add(cfg.roles.unregister).catch();
+     await uye.guild.channels.cache.get(cfg.chats.kayıtChat)
+        .send(
+        `${uye} adlı üyenin hesabı 1 haftadan önce kurulmasına rağmen \`şüpheli\` rolü bulunamadığı için unregister permi verildi`
+      ).catch();
+      return;
+    } else {
+      await uye.roles.add(fakeRol).catch();
+      await uye.send(
+        `Hesabın 1 haftadan önce açıldığı için *şüpheli* kısmına atıldın. Eğer Discordda yeni isen sağ üstten birisine ulaşarak sunucuya giriş yapabilirsin.`
+      ).catch(err => uye.guild.channels.cache.get(cfg.chats.kayıtChat).send(`${uye} adlı üyenin hesabı 1 hafta önceden açıldığı için şüpheli permi verildi.`));
+    };
+  } else {
+    await uye.roles.add(cfg.roles.unregister);
+    await uye.guild.channels.cache.get(cfg.chats.kayıtChat).send(
+      `text`
+    ).catch();
+    
+  };
+};
 
 module.exports.help = { name: "guildMemberAdd" };
