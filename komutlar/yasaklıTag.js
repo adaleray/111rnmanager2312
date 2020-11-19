5547ff663d7dd772f69aea3b3bf774176c07fbf6
Glitch (hello-express) <none>
2020-11-19 15:43:46 +0000

    📌💡 Checkpoint ./komutlar/yasaklıTag.js:14391105/1424

--- komutlar/yasaklıTag.js
+++ komutlar/yasaklıTag.js
@@ -1,9 +1,29 @@
 module.exports.execute = async ({client, msg, author, args, db, cfg}) => {
   if (!cfg.sahipler.includes(author.id)) return;
   
+  var evet = "✅";
+  var hayir = "❌";
+  
   let type = args[0].toLowerCase();
   
   if (type === "rol-ekle") {
+    const rol = msg.mentions.roles.first() || msg.guild.roles.cache.get(args[1]);
+    function onlarFilterBenBeko(r, u) { return [evet, hayir].includes(r.emoji.name) && u.id === msg.author.id };
+    if (!rol) return msg.channel.send("**Bir rol etiketlemeli veya idsini girmelisin.**").then(m => m.delete({ timeout: 5000 }));
+    
+    if (db.get(`yasakliTagRol_${msg.guild.id}`)) {
+      msg.channel.send({
+        embed: { 
+          description: `Yasaklı tag komutu için zaten bir role sahipsin. Onaylıyorsan kayıtlı rol yerine ${rol} rolünü yasaklı taga koyacağım.`,
+          timestamp: new Date(),
+          color:Math.floor(Math.random() * (0xFFFFFF + 1))
+        }
+      }).then(async (m) => {
+        await m.react(evet);
+        await m.react(hayir);
+        
+      });
+    }
     
   } else if (type === "rol-sil") {
     
