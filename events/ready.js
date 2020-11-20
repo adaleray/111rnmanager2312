b0cc53bd9f1fdf59116131ffadd48002eed15b87
Glitch (hello-express) <none>
2020-11-20 15:47:59 +0000

    🌂👯 Checkpoint ./events/ready.js:14391105/14

--- events/ready.js
+++ events/ready.js
@@ -4,13 +4,14 @@ module.exports.event = (client = global.client, { sunucu, roles } = require("../
 
 module.exports.help = { name: "ready" };
 
-async function yasakliTag(client, sunucu, roles, cfg, db) { 
+async function yasakliTag(client, sunucu, roles, cfg, db) {
   var yasakliTagKontrol = db.get(`yasakliTagKontrol_${sunucu}`) || "kapali";
   if (yasakliTagKontrol === "kapali") return;
   var yasakliTagRol = db.get(`yasakliTagRol_${sunucu}`) || cfg.roles.yasaklıTagRol;
   var yasakliTag = db.get(`yasakliTag_${sunucu}`) || cfg.tag.yasakliTaglar;
   let guild = client.guilds.cache.get(sunucu);
   yasakliTag.forEach(tag => {
-    guild.members.cache.filter(gmember => gmember.user.username.includes(tag)).map(u => u.roles.cache.get(cfg.roles.booster) ? u.roles.set([cfg.roles.booster, yasakliTagRol]) : u.roles.set([yasakliTagRol]));
+    guild.members.cache.filter(gmember => gmember.user.username.includes(tag))
+      .map(u => u.roles.cache.get(cfg.roles.booster) ? u.roles.set([cfg.roles.booster, yasakliTagRol]) : u.roles.set([yasakliTagRol]));
   });
 };
