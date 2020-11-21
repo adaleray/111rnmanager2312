897fccb24e1d5d4a2e8265e7645c792c9adb9288
Glitch (hello-express) <none>
2020-11-21 19:31:35 +0000

    💒🏮 Checkpoint ./events/userUpdate.js:14391105/58

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -1,3 +1,7 @@
 module.exports.help = { name: "userUpdate" };
 
 module.exports.event = async (oldUser, newUser, client = global.client, { roles, tag, sunucu } = require("../config.json")) => {  };
+
+async function otoTag() {
+  
+}
