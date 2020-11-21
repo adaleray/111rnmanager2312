beaed2eb5d9d3572ca467989a4d7ce3f3a193b21
Glitch (hello-express) <none>
2020-11-21 14:47:01 +0000

    🚴🏃 Checkpoint ./events/userUpdate.js:14391105/369

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -1,5 +1,12 @@
-module.exports.event = (oldUser, newUser, client = global.client, { roles, tag } = require("../config.json")) => {
-  
-};
+module.exports.event = (
+  oldUser,
+  newUser,
+  client = global.client,
+  { roles, tag } = require("../config.json")
+) => {};
+
+class otoTag {
+  consturctor(oldUser, newUser, client, roles, tag)
+}
 
 module.exports.help = { name: "userUpdate" };
