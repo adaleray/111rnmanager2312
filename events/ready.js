eaf1cd32f96b468886a7b7802f63ee71934dc635
Glitch (hello-express) <none>
2020-11-21 20:06:24 +0000

    🚦👑 Checkpoint ./events/ready.js:14391105/5209

--- events/ready.js
+++ events/ready.js
@@ -1,50 +1,78 @@
-module.exports.help = { name: "ready" };
+class Login {
+  constructor(client) {
+    this.client = client;
+  }
+  log(guild) {
+    this.client.user.setStatus("idle");
+    console.log("(" +this.client.user.username +") adlı hesapta [" +guild.name +"] adlı sunucuda giriş yapıldı.");
+  }
+}
 
-class Login { constructor(client) { this.client = client; } log(guild) { this.client.user.setStatus("idle"); console.log("(" + this.client.user.username + ") adlı hesapta [" + guild.name + "] adlı sunucuda giriş yapıldı."); } }
+class YasakliTag {
+  constructor(client, sunucu, roles, cfg, db) {
+    this.client = client;
+    this.sunucu = sunucu;
+    this.roles = roles;
+    this.cfg = cfg;
+    this.db = db;
+  }
+  
+  tagKontrol() {
+    var yasakliTagKontrol = this.db.get(`yasakliTagKontrol_${this.sunucu}`) || "kapali";
+    if (yasakliTagKontrol === "kapali") return;
+    var yasakliTagRol = this.db.get(`yasakliTagRol_${this.sunucu}`) || this.cfg.roles.yasaklıTagRol;
+    var yasakliTag = this.db.get(`yasakliTag_${this.sunucu}`) || this.tag.yasakliTaglar;
+    let guild = this.client.guilds.cache.get(this.sunucu);
+    yasakliTag.forEach(tag => {
+    guild.members.cache.filter(gmember => gmember.user.username.includes(tag))
+      .map(u => u.roles.cache.get(this.cfg.roles.booster) ? u.roles.set([this.cfg.roles.booster, yasakliTagRol]) : u.roles.set([yasakliTagRol]));
+    });
+  }
+}
+
+class ChatEdit {
+  constructor(client, sunucu, chat, sncIsim, tagrolIsim, tag) {
+    this.client = client;
+    this.sunucu = sunucu;
+    this.chat = chat;
+    this.sncIsim = sncIsim;
+    this.tagrolIsim = tagrolIsim;
+    this.tag = tag;
+  }
+  edit() {
+    let guild = this.client.guilds.cache.get(this.sunucu);
+    let kanal = guild.channels.cache.get(this.chat);
+    if (!kanal) return null;
+    let cookie = 0;
+    let sayi = guild.memberCount;
+    let taglı = guild.members.cache.filter(u => u.user.username.includes(this.tag)).size;
+    let online = guild.members.cache.filter(u => u.presence.status !== "offline").size;
+      cookie = cookie + 1;
+      if (cookie === 1) {
+        kanal.edit({
+             topic: `**${this.sncIsim}: ${this.client.emojili(sayi)} Online: ${this.client.emojili(online)} ${this.tagrolIsim}: ${this.client.emojili(taglı)}**`
+        });
+      } else if (cookie === 2) {
+        kanal.edit({
+            topic: `**${this.sncIsim}: ${this.client.emojili(sayi)} Online: ${this.client.emojili(online)} ${this.tagrolIsim}: ${this.client.emojili(taglı)}**`
+        });
+      } else if (cookie === 3) {
+        kanal.edit({
+            topic: `**${this.sncIsim}: ${this.client.emojili(sayi)} Online: ${this.client.emojili(online)} ${this.tagrolIsim}: ${this.client.emojili(taglı)}**`
+        });
+      } else if (cookie === 4) {
+        cookie = 0;
+        kanal.edit({
+            topic: `**${this.sncIsim}: ${this.client.emojili(sayi)} Online: ${this.client.emojili(online)} ${this.tagrolIsim}: ${this.client.emojili(taglı)}**`
+        });
+      };
+  }
+}
+
+module.exports.help = { name: "ready" };
 
 module.exports.event = (client = global.client, { sunucu, roles } = require("../config.json"), cfg = require("../config.json"), db = require("quick.db")) => {
   new Login(client).log(client.guilds.cache.get(sunucu));
-  setInterval(() => yasakliTag(client, sunucu, roles, cfg, db), client.getDate(2, "saat"));
-  setInterval(() => chatEdit(client, sunucu, cfg.chats.gchat, cfg.snc.sncIsim, cfg.snc.tagRolIsim, cfg.tag.taglıTag), client.getDate(30, "dakika"));
-};
-
-async function yasakliTag(client, sunucu, roles, cfg, db) {
-  var yasakliTagKontrol = db.get(`yasakliTagKontrol_${sunucu}`) || "kapali";
-  if (yasakliTagKontrol === "kapali") return;
-  var yasakliTagRol = db.get(`yasakliTagRol_${sunucu}`) || cfg.roles.yasaklıTagRol;
-  var yasakliTag = db.get(`yasakliTag_${sunucu}`) || cfg.tag.yasakliTaglar;
-  let guild = client.guilds.cache.get(sunucu);
-  yasakliTag.forEach(tag => {
-    guild.members.cache.filter(gmember => gmember.user.username.includes(tag))
-      .map(u => u.roles.cache.get(cfg.roles.booster) ? u.roles.set([cfg.roles.booster, yasakliTagRol]) : u.roles.set([yasakliTagRol]));
-  });
-};
-
-async function chatEdit(client, sunucu, chat, sncIsim, tagrolIsim, tag) {
-  let guild = client.guilds.cache.get(sunucu);
-  let kanal = guild.channels.cache.get(chat);
-  if (!kanal) return null;
-  let cookie = 0;
-  let sayi = guild.memberCount;
-  let taglı = guild.members.cache.filter(u => u.user.username.includes(tag)).size;
-  let online = guild.members.cache.filter(u => u.presence.status !== "offline").size;
-    cookie = cookie + 1;
-    if (cookie === 1) {
-      kanal.edit({
-           topic: `**${sncIsim}: ${client.emojili(sayi)} Online: ${client.emojili(online)} ${tagrolIsim}: ${client.emojili(taglı)}**`
-      });
-    } else if (cookie === 2) {
-      kanal.edit({
-          topic: `**${sncIsim}: ${client.emojili(sayi)} Online: ${client.emojili(online)} ${tagrolIsim}: ${client.emojili(taglı)}**`
-      });
-    } else if (cookie === 3) {
-      kanal.edit({
-          topic: `**${sncIsim}: ${client.emojili(sayi)} Online: ${client.emojili(online)} ${tagrolIsim}: ${client.emojili(taglı)}**`
-      });
-    } else if (cookie === 4) {
-      cookie = 0;
-      kanal.edit({
-          topic: `**${sncIsim}: ${client.emojili(sayi)} Online: ${client.emojili(online)} ${tagrolIsim}: ${client.emojili(taglı)}**`
-      });
-    };
+  setInterval(() => new YasakliTag(client, sunucu, roles, cfg, db), client.getDate(2, "saat")).tagKontrol();
+  setInterval(() => new ChatEdit(client, sunucu, cfg.chats.gchat, cfg.snc.sncIsim, cfg.snc.tagRolIsim, cfg.tag.taglıTag), client.getDate(30, "dakika")).edit();
 };
