e9646e378b731beb311da7c11959af02f527a3ee
Glitch (hello-express) <none>
2020-11-22 13:20:28 +0000

    🏁💐 Checkpoint ./komutlar/say.js:14391105/427 ./config.json:14391105/3

--- komutlar/say.js
+++ komutlar/say.js
@@ -3,9 +3,9 @@ module.exports.execute = async ({msg, author, args, client, cfg, db}) => {
   let sayi = 0;
   const sayTürü = db.get(`sayTuru_${msg.guild.id}`) || "emojisiz";
   const uyeSayisi = msg.guild.memberCount;
-  const tagliUye = msg.guild.members.cache.filter(u => u.user.username.includes(cfg.tag.taglıTag)).size || "Rol Belirtilmedi.";
+  const tagliUye = msg.guild.members.cache.filter(u => u.user.username.includes(cfg.tag.taglıTag)).size || 0;
   const onlineUye = msg.guild.members.cache.filter(u => u.presence.status !== "offline").size;
-  const boosterUye = msg.guild.members.cache.filter(u => u.roles.cache.get(cfg.roles.booster)).size || "Rol Belirtilmedi.";
+  const boosterUye = msg.guild.members.cache.filter(u => u.roles.cache.get(cfg.roles.booster)).size || "Rol Belirtilmedi";
   const type = args[0];
   msg.guild.channels.cache.filter(c => c.type === "voice").map(k => { 
     sayi += k.members.size
@@ -36,7 +36,7 @@ module.exports.execute = async ({msg, author, args, client, cfg, db}) => {
       });
     } else {
       msg.channel.send(
-        `**${cfg.snc.sncIsim}: ${client.emojili(uyeSayisi)}               Online: ${client.emojili(onlineUye)}**\n\n                    **${cfg.snc.tagRolIsim}: ${client.emojili(tagliUye)}**`
+        `**${cfg.snc.sncIsim}: ${client.emojili(uyeSayisi)}               Online: ${client.emojili(onlineUye)}**\n\n            **${cfg.snc.tagRolIsim}: ${client.emojili(tagliUye)}**`
       );
     };
   } else if (type === "ayarla") {
@@ -45,10 +45,10 @@ module.exports.execute = async ({msg, author, args, client, cfg, db}) => {
     if (ayar === "emojili") {
       if (sayTürü === "emojili") return msg.channel.send("**Say türü zaten emojili.**").then(m => m.delete({ timeout: 5000 }));
       await db.set(`sayTuru_${msg.guild.id}`, "emojili");
-      await msg.channel.send({embed:{description:`**Say türü emojili olarak değiştirildi.**`, color:Math.floor(Math.random()*(0xFFFFFF+1)), timestamp:new Date()}}).then(m => m.delete({timeout:5000}));
+      msg.channel.send({embed:{author:{icon_url:msg.guild.iconURL({dynamic:true}),name:msg.guild.name},description:`**Say türü emojili olarak değiştirildi.**`, color:Math.floor(Math.random()*(0xFFFFFF+1)), timestamp:new Date()}}).then(m => m.delete({timeout:5000}));
     } else if (ayar === "emojisiz") {
       await db.set(`sayTuru_${msg.guild.id}`, "emojisiz");
-      await msg.channel.send({embed:{description:`**Say türü emojisiz olarak değiştirildi.**`, color:Math.floor(Math.random()*(0xFFFFFF+1)), timestamp:new Date()}}).then(m => m.delete({timeout:5000}));
+      msg.channel.send({embed:{author:{icon_url:msg.guild.iconURL({dynamic:true}),name:msg.guild.name},description:`**Say türü emojisiz olarak değiştirildi.**`, color:Math.floor(Math.random()*(0xFFFFFF+1)), timestamp:new Date()}}).then(m => m.delete({timeout:5000}));
     } else return msg.channel.send("**Say türü ayarlarken sadece**\n\n`emojili` veya `emojisiz` olarak ayarlanabilir.").then(m => m.delete({timeout:5000}));
   };
 };
