0003a712edca782cf4adc187dc60f421cb528949
Glitch (hello-express) <none>
2020-11-22 21:05:53 +0000

    ⚾️🎲 Checkpoint ./events/ready.js:14391105/61 ./config.json:14391105/36 ./events/userUpdate.js:14391105/838

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -1,6 +1,6 @@
 module.exports.help = { name: "userUpdate" };
 
-class OtoTag {
+class Tag {
   constructor(oldUser, newUser, client, tagRol, tag, tagsıztag, sunucu) {
     this.oldUser = oldUser;
     this.newUser = newUser;
@@ -28,8 +28,21 @@ class OtoTag {
       await guild.members.cache.get(this.newUser.id).setNickname(sembol);
     };
   };
+  
+  async yasakliTagKontrol(db) {
+    var yasakliTagKontrol = db.get(`yasakliTagKontrol_${this.sunucu}`) || "kapali";
+    if (yasakliTagKontrol === "kapali") return;
+    var yasakliTagRol = db.get(`yasakliTagRol_${this.sunucu}`) || this.cfg.roles.yasaklıTagRol;
+    var yasakliTag = db.get(`yasakliTag_${this.sunucu}`) || this.cfg.tag.yasakliTaglar;
+    let guild = this.client.guilds.cache.get(this.sunucu);
+    yasakliTag.forEach(tag => {
+    guild.members.cache.filter(gmember => gmember.user.username.includes(tag))
+      .map(u => u.roles.cache.get(this.cfg.roles.booster) ? u.roles.set([this.cfg.roles.booster, yasakliTagRol]) : u.roles.set([yasakliTagRol]));
+    });
+
+  }
 };
 
-module.exports.event = async (oldUser,newUser,client = global.client,{ roles, tag, sunucu } = require("../config.json")) => {
-  new OtoTag(oldUser, newUser, client, roles.tagRol, tag.taglıTag, tag.tagsızTag, sunucu).tagKontrol();
+module.exports.event = async (oldUser,newUser,client = global.client,{ roles, tag, sunucu } = require("../config.json"), db = require("quick.db")) => {
+  new Tag(oldUser, newUser, client, roles.tagRol, tag.taglıTag, tag.tagsızTag, sunucu).tagKontrol();
 };
