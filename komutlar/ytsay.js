7ae92ea2cb8d179c24a2cb2f4f7c7046153daf1b
Glitch (hello-express) <none>
2020-11-22 19:34:22 +0000

    🚡🌕 Checkpoint ./komutlar/ytsay.js:14391105/7 ./config.json:14391105/44

--- komutlar/ytsay.js
+++ komutlar/ytsay.js
@@ -3,14 +3,14 @@ module.exports.operate = ({msg, args, author, cfg}) => {
   if (!args[0]) {
     let uyeler = msg.guild.members.cache.filter(u => {
       return (
-        u.roles.cache.some(r => roles.yetkiliRoller.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline"
+        u.roles.cache.some(r => cfg.roles.yetkiliRoller.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline"
       )
     }).map(u => u.user);
     msg.channel.send(`**Aktif olup seste olmayan yetkililer : \n \n** ${uyeler.join(", \n")}`);
   } else if (["dm", "dm-at"].includes(args[0])) {
     msg.guild.members.cache.filter(u => {
       return (
-        u.roles.cache.some(r => roles.yetkiliRoller.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline"
+        u.roles.cache.some(r => cfg.roles.yetkiliRoller.includes(r.id)) && !u.voice.channel && u.presence.status !== "offline"
       );
     }).forEach(uye => {
       var text = "Merhabalar sunucumuzun ses aktifliğini arttırmak için lütfen müsait isen public odalara değil isen alone odalarına geçer misin?";
