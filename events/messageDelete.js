051698c16e36f196b8ad24ef5e5337e88bfdf48a
Glitch (hello-express) <none>
2020-11-22 18:25:12 +0000

    💧🍶 Checkpoint ./events/messageEdit.js:14391105/1548 ./events/messageDelete.js:14391105/277

---
+++ events/messageDelete.js
@@ -0,0 +1,18 @@
+module.exports.help = { name: "messageDelete" };
+
+class MessageDelete {
+  constructor(msg, sunucu) {
+    this.msg = msg;
+    this.sunucu = sunucu;
+  }
+  
+  logla(kanal) {
+    
+  }
+  
+  snipe(db) {
+    
+  }
+}
+
+module.exports.event = () => {};
--- events/messageEdit.js
+++ events/messageEdit.js
@@ -1,14 +1,11 @@
 module.exports.help = { name: "messageUpdate" };
 
 class MessageUpdate {
-  constructor(oldMessage, newMessage, sunucu, client, cfg) {
+  constructor(oldMessage, newMessage, sunucu) {
     this.oldMessage = oldMessage;
     this.newMessage = newMessage;
     this.sunucu = sunucu;
-    this.cfg = cfg;
-    this.client = client;
   }
-  
   logla(kanal) {
     if (this.newMessage.author.bot || this.newMessage.channel.type === "dm") return;
     if (this.newMessage.guild.id !== this.sunucu) return;
@@ -36,29 +33,9 @@ class MessageUpdate {
       }
     });
   }
-  async getCommand(prefix, args, command) {
-    if (!this.newMessage.toLowerCase().startsWith(prefix)) return;
-    let cmd;
-    let author = this.newMessage.guild.member(this.newMessage.author);
-    let uye = this.newMessage.guild.member(this.newMessage.mentions.users.first());
-    if (this.client.commands.has(command)) {
-      cmd = this.client.commands.get(command);
-    } else if (this.client.aliases.has(command)) {
-      cmd = this.client.commands.get(this.client.aliases.get(command));
-    };
-    if (cmd) {
-      cmd.operate({ client: this.client, msg: this.newMessage, args: args, author: author, uye: uye, cfg: this.cfg, db: require("quick.db") });
-    };
-  }
 }
 
 module.exports.event = (oldMessage, newMessage, { sunucu } = require("../config.json"), cfg = require("../config.json") ,client = global.client, db = require("quick.db")) => {
   let messageLog = newMessage.guild.channels.cache.find(c => c.name === "message-log");
-  let prefixMention = new RegExp(`^<@!${client.user.id}>`);
-  let pref = newMessage.content.match(prefixMention) ? newMessage.content.match(prefixMention)[0] : cfg.prefix.toLowerCase();
-  if (!newMessage.content.toLowerCase().startsWith(cfg.prefix)) return;
-  let args = newMessage.content.slice(cfg.prefix.length).trim().split(/ +/g);
-  let command = args.shift().toLowerCase();
   new MessageUpdate(oldMessage, newMessage, sunucu, client).logla(messageLog);
-  new MessageUpdate(oldMessage, newMessage, sunucu, client).getCommand(pref, args, command);
 };
