2de4ada382f40235e2cd086610af54fd98f65930
Glitch (hello-express) <none>
2020-11-22 12:46:19 +0000

    🤑👙 Checkpoint ./komutlar/say.js:14391105/2358

--- komutlar/say.js
+++ komutlar/say.js
@@ -1,8 +1,17 @@
-module.exports.execute = async ({msg, author, client, cfg}) => {
+module.exports.execute = async ({msg, author, client, cfg, db}) => {
   if (!author.permissions.has("MANAGE_ROLES")) return;
-  let uyeSayisi = msg.guild.memberCount;
-  let tagliUye = msg.guild.members.cache.filter(u => u.user.username.includes(cfg.tag.taglıTag)).size;
-  let onlineUye = msg.guild.members.cache.filter(u => u.presence.status !== "offline").size;
+  const sayTürü = db.get(`sayTuru_${msg.guild.id}`) || "emojisiz";
+  const uyeSayisi = msg.guild.memberCount;
+  const tagliUye = msg.guild.members.cache.filter(u => u.user.username.includes(cfg.tag.taglıTag)).size;
+  const onlineUye = msg.guild.members.cache.filter(u => u.presence.status !== "offline").size;
+
+  if (sayTürü === "emojisiz") {
+    
+  } else {
+    msg.channel.send(
+      `**${cfg.snc.sncIsim}: ${client.emojili(uyeSayisi)}               Online: ${client.emojili(onlineUye)}**\n\n                    **${cfg.snc.tagRolIsim}: ${client.emojili(tagliUye)}**`
+    );
+  };
 };
 
 module.exports.help = {
