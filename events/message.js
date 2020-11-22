cf877e29a94a1ae011a95e8c1f7cd740160e1760
Glitch (hello-express) <none>
2020-11-22 16:40:10 +0000

    🚿🍙 Checkpoint ./komutlar/ban.js:14391105/573 ./events/message.js:14391105/1681 ./config.json:14391105/1637

--- events/message.js
+++ events/message.js
@@ -1,5 +1,7 @@
 module.exports.event = (msg, client = global.client, cfg = require("../config.json")) => {
   if (msg.content.includes("discord.gg") && !author.permissions.has("MANAGE_ROLES")) return msg.guild.members.ban(msg.author, { days: 7, reason: "oc" });
+  if (msg.content.toLowerCase().startsWith("!tag")) return msg.channel.send(cfg.tag.taglıTag);
+  if (msg.content.toLowerCase().startsWith("!link")) return msg.channel.send(cfg.link);
   let prefixMention = new RegExp(`^<@!${client.user.id}>`);
   let pref = msg.content.match(prefixMention) ? msg.content.match(prefixMention)[0] : cfg.prefix.toLowerCase();
   if (!msg.content.toLowerCase().startsWith(cfg.prefix)) return;
--- komutlar/ban.js
+++ komutlar/ban.js
@@ -7,12 +7,9 @@ module.exports.operate = ({msg, client, uye, author, args, db, cfg}) => {
   if (!author.permissions.has("BAN_MEMBERS") && !author.roles.cache.get(cfg.roles.banH)) return msg.channel.send("**Gerekli yetkiye sahip değilsin.**").then(m => m.delete({ timeout: 5000 }));
   if (!uye) return msg.channel.send("**Bir üyeyi etiketlemelisin.**").then(x => x.delete({ timeout: 5000 }));
   if (uye.permissions.has("BAN_MEMBERS") && uye.roles.cache.get(cfg.roles.banH)) return msg.channel.send("**Ban yetkisi olan birisini banlayamazsın.**").then(a => a.delete({ timeout: 5000 }));
-  if (!banAtanlar[msg.author.id])
-    banAtanlar[msg.author.id] = {
-      sayi: 0
-    };
+  if (!banAtanlar[msg.author.id]) banAtanlar[msg.author.id] = { sayi: 0 };
   if (banAtanlar[msg.author.id].sayi >= 3) return msg.channel.send("**1 saat içinde maximum 3 ban atabilirsin.**").then(a => a.delete({ timeout: 5000 }));
-  let reason = args[1] ? args.slice(1).join(" ") : "Sebep Girilmedi.";
+  let reason = args.slice(1).join(" ") || "Sebep Girilmedi.";
   db.set(`banSebebi_${uye.id}, ${msg.guild.id}`, reason);
   db.set(`banAtan_${uye.id}, ${msg.guild.id}`, msg.author.tag);
   db.set(`banZamani_${uye.id}, ${msg.guild.id}`, tarih);
@@ -21,19 +18,14 @@ module.exports.operate = ({msg, client, uye, author, args, db, cfg}) => {
     reason: reason,
     days: 7
   });
-    const embed = new MessageEmbed()
-      .setDescription(`**${uye.user.tag}** adlı üye ${msg.author} tarafından **${reason}** sebebiyle sunucudan yasaklandı`)
-      .setColor("BLACK")
-      .setTimestamp()
-    msg.channel.send({embed:{description:``}})
-    msg.channel.send(embed);
-    banAtanlar[msg.author.id].sayi++;
-    setTimeout(() => {
-      if (banAtanlar[msg.author.id].sayi >= 1) {
-        banAtanlar[msg.author.id].sayi = 0;
-      };
-    }, 3600000);
-}
+  msg.channel.send({embed:{description:`**${uye.user.tag}** adlı üye ${msg.author} tarafından **${reason}** sebebiyle sunucudan yasaklandı`, color:Math.floor(Math.random()*(0xFFFFFF + 1)), timestamp: new Date()}});
+  banAtanlar[msg.author.id].sayi++;
+  setTimeout(() => {
+    if (banAtanlar[msg.author.id].sayi >= 1) {
+      banAtanlar[msg.author.id].sayi = 0;
+    };
+  }, 3600000);
+};
 
 module.exports.help = {
   name: "ban",
