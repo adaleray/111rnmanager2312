7f177a86d5d703bbc275ee7949ae1361fab7d320
Glitch (hello-express) <none>
2020-11-23 19:28:57 +0000

    🔮🌁 Checkpoint ./komutlar/unregister.js:14391105/598 ./komutlar/ban.js:14391105/195 ./komutlar/isimler.js:14391105/2

--- komutlar/ban.js
+++ komutlar/ban.js
@@ -7,6 +7,7 @@ module.exports.operate = async ({msg, client, uye, author, args, db, cfg}) => {
   if (!author.permissions.has("BAN_MEMBERS") && !author.roles.cache.get(cfg.roles.banH)) return msg.channel.send("**Gerekli yetkiye sahip değilsin.**").then(m => m.delete({ timeout: 5000 }));
   if (!uye) return msg.channel.send("**Bir üyeyi etiketlemelisin.**").then(x => x.delete({ timeout: 5000 }));
   if (uye.permissions.has("BAN_MEMBERS") && uye.roles.cache.get(cfg.roles.banH)) return msg.channel.send("**Ban yetkisi olan birisini banlayamazsın.**").then(a => a.delete({ timeout: 5000 }));
+  if (author.roles.highest.position <= uye.roles.highest.position) return msg.channel.send("**Belirttiğin kişi senden üstün veya onunla aynı yetkidesin!**").then(x => x.delete({timeout: 5000}));
   if (!banAtanlar[msg.author.id]) banAtanlar[msg.author.id] = { sayi: 0 };
   if (banAtanlar[msg.author.id].sayi >= 3) return msg.channel.send("**1 saat içinde maximum 3 ban atabilirsin.**").then(a => a.delete({ timeout: 5000 }));
   let reason = args.slice(1).join(" ") || "Sebep Girilmedi.";
--- komutlar/unregister.js
+++ komutlar/unregister.js
@@ -1,6 +1,9 @@
 module.exports.operate = async ({client, msg, args, author, uye, cfg}) => {
   if (!author.permissions.has("MANAGE_ROLES")) return;
-  if (uye.roles.cache.get(cfg.roles.botc) || || uye.permissions.has("MANAGE_ROLES")) return msg.channel.send("**Bir yetkiliyi unregistere atamazsın.**").then(m => m.delete({ timeout: 5000 }));
+  if (uye.roles.cache.get(cfg.roles.botc) || uye.roles.cache.get(cfg.roles.booster) || uye.permissions.has("MANAGE_ROLES")) return msg.channel.send("**Bir yetkiliyi veya boosteri unregistere atamazsın.**").then(m => m.delete({ timeout: 5000 }));
+  if (author.roles.highest.position <= uye.roles.highest.position) return msg.channel.send("**Belirttiğin kişi senden üstün veya onunla aynı yetkidesin!**").then(x => x.delete({timeout: 5000}));
+  await uye.roles.set([cfg.roles.unregister]);
+  await msg.channel.send(client.nrmlembed(`${uye}** adlı üye başarıyla kayıtsıza atıldı.**`)).then(msj => msj.delete({ timeout: 2000 }));
 };
 
 module.exports.help = {
