5fab2e5919eb21050e95e45586ff06a741040bca
Glitch (hello-express) <none>
2020-11-23 09:53:41 +0000

    💑🐃 Checkpoint ./komutlar/jail.js:14391105/104

--- komutlar/jail.js
+++ komutlar/jail.js
@@ -1,6 +1,8 @@
 module.exports.operate = async ({client, msg, args, author, uye, cfg, db}) => {
   if (!author.permissions.has("MANAGE_ROLES") && !author.roles.cache.get(cfg.roles.jailH)) return msg.channel.send("**Gerekli yetkiye sahip değilsin.**").then(m => m.delete({timeout: 5000}));
   
+  const jailSicil = db.get(`jailSicil_${uye.id}`) || [];
+  
 };
 
 module.exports.help = {
