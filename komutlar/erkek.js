4ec7a75cad2c9dbb22d9900939ded84472f88a1f
Glitch (hello-express) <none>
2020-11-23 15:30:27 +0000

    🏁🏃 Checkpoint ./LONA.js:14391105/752 ./komutlar/erkek.js:14391105/323

--- komutlar/erkek.js
+++ komutlar/erkek.js
@@ -1,5 +1,6 @@
 module.exports.operate = async ({client, msg, args, author, uye, cfg, db}) => {
-  
+  if (!author.roles.cache.get(cfg.roles.botc) && !author.permissions.has("MANAGE_ROLES")) return msg.channel.send("**Gerekli yetkiye sahip değilsin.**").then(m => m.delete({ timeout: 5000 }));
+  if (!uye) return msg.channel.send("**Bir üye etiketlemelisin.**").then(msj => msj.delete({timeout: 5000 }));
 };
 
 module.exports.help = {
