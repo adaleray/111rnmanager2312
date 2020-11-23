4c3982be089ab3e14bb249420e2603cfa830cd51
Glitch (hello-express) <none>
2020-11-23 18:53:56 +0000

    ❣️📕 Checkpoint ./komutlar/ytbilgi.js:14391105/1128 ./komutlar/karı.js:14391105/104 ./komutlar/erkek.js:14391105/180

--- komutlar/ytbilgi.js
+++ komutlar/ytbilgi.js
@@ -1,5 +1,16 @@
 module.exports.operate = async ({client, msg, args, author, uye, cfg, db}) => {
+  if (!author.roles.cache.get(cfg.roles.botc) && !author.permissions.has("MANAGE_ROLES")) return;
+  var kullanici = author || uye;
   
+  const erkek = db.get(`erkekTeyit_${kullanici.id}`) || 0;
+  const kız = db.get(`karıTeyit_${kullanici.id}`) || 0;
+  const jail = db.get(`jailAtma_${kullanici.id}`) || 0;
+  const mute = db.get(`muteAtma_${kullanici.id}`) || 0;
+  const ban = db.get(`banAtma_${kullanici.id}`) || 0;
+
+  msg.channel.send({
+    embed: { author: {} }
+  })
 };
 
 module.exports.help = {
