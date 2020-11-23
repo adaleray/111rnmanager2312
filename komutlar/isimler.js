b0384e8118de91f72a587fef75daaa789c55eef4
Glitch (hello-express) <none>
2020-11-23 17:08:10 +0000

    🚔🚥 Checkpoint ./komutlar/isimler.js:14391105/806

--- komutlar/isimler.js
+++ komutlar/isimler.js
@@ -2,9 +2,9 @@ module.exports.operate = async ({client, msg, args, author, uye, cfg, db}) => {
   if (!author.roles.cache.get(cfg.roles.botc) && !author.permissions.has("MANAGE_ROLES")) return msg.channel.send("**Gerekli yetkiye sahip değilsin.**").then(m => m.delete({ timeout: 5000 }));
   if (!uye) return msg.channel.send("**Bir üye etiketlemelisin.**").then(m => m.delete({ timeout: 5000 }));
   const isimler = db.get(`isimler_${uye.id}`) || ["Kayıtlı isim yok !"];
-  msg.channel.send(isimler.join(", "))
+  msg.channel.send();
 };
-//sa abi as
+
 module.exports.help = {
   name: "isimler",
   alias: ["kayıtlar"]
