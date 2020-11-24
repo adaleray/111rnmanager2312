3a948e8ba50f91b58c5fe4f7104b55d8072389bd
Glitch (hello-express) <none>
2020-11-24 15:49:33 +0000

    🦂🏕 Checkpoint ./komutlar/topluçek.js:14391105/1054:22440010/11108

--- komutlar/topluçek.js
+++ komutlar/topluçek.js
@@ -1,15 +1,9 @@
 module.exports.operate = async ({client, msg, args, author, uye, cfg}) => {
   if (!author.roles.cache.get(cfg.roles.transport) && !author.permissions.has("ADMINISTRATOR")) return msg.channel.send('**Gerekli yetkiye sahip değilsin.**').then(m => m.delete({ timeout: 3000 }));
-  const channel = args[0];
-  if (!channel) {
-    
-  } else if (channel) {
-    var uyeler = msg.guild.channels.cache.get(channel).members.cache.array();
-    let gidilecek = args[1];
-    if (!gidilecek) return msg.channel.send("**Gitmek istediğin bir kanal idsi belirtmedin.**").then(msj => msj.delete({ timeout: 5000 }));
-    await msg.channel.send(client.nrmlembed(`**Başarıyla \`${uyeler.size}\` üyeyi <#${channel}> kanalından <#${gidilecek}> kanalına taşıdım.**`)).then(msj => msj.delete({ timeout: 5000 }));
-    uyeler.map(uye => uye.voice.setChannel(gidilecek));
-  } else return;
+  const channel = args[1];
+  if (channel) {
+    var uyeler = msg.guild.members.cache.filter(uye => msg.guild.members.cache.get(uye.id).voice.channel.id === args[0])
+  };
 };
 
 module.exports.help = {
