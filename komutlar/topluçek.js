079e3d85893fe5ad11970dbb22b0429458a04e4f
Glitch (hello-express) <none>
2020-11-24 16:01:16 +0000

    📒⚡️ Checkpoint ./KAÇ_SATIR_KOD_YAZDIM.js:22440010/11328 ./.glitch-assets:22440010/522 ./komutlar/topluçek.js:14391105/172

--- komutlar/topluçek.js
+++ komutlar/topluçek.js
@@ -1,9 +1,7 @@
 module.exports.operate = async ({client, msg, args, author, uye, cfg}) => {
   if (!author.roles.cache.get(cfg.roles.transport) && !author.permissions.has("ADMINISTRATOR")) return msg.channel.send('**Gerekli yetkiye sahip değilsin.**').then(m => m.delete({ timeout: 3000 }));
-  const channel = args[1];
-  if (channel) {
-    var uyeler = msg.guild.members.cache.filter(uye => msg.guild.members.cache.get(uye.id).voice.channel.id === args[0])
-  };
+
+  
 };
 
 module.exports.help = {
