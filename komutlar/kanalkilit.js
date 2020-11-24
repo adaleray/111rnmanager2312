3f83d5c399afdc51cb8d1b9be13c8571bf281c72
Glitch (hello-express) <none>
2020-11-24 02:28:41 +0000

    ⛽️🌁 Checkpoint ./komutlar/kanalkilit.js:22440010/3572 ./komutlar/seslikontrol.js:22440010/802 ./komutlar/seskontrol.js:22440010/36 ./komutlar/unban.js:22440010/1168

---
+++ komutlar/kanalkilit.js
@@ -0,0 +1,25 @@
+module.exports.operate = async ({msg, author}, locked = new Set()) => {
+ 
+  if (!author.permissions.has("ADMINISTRATOR")) return;
+  if (!locked[msg.channel.id]) locked[msg.channel.id] = {
+    lock: false
+  };
+  if (locked[msg.channel.id].lock === false) {
+    msg.channel.send({ embed: { description: "**Kanal başarıyla kilitlendi.**", color: Math.floor(Math.random() * (0xFFFFFF + 1)), timestamp: new Date()}});
+    msg.channel.updateOverwrite(msg.guild.roles.everyone, {  
+      SEND_MESSAGES: false
+    });   
+    locked[msg.channel.id].lock = true;
+  } else {
+    msg.channel.send({ embed: { description: "**Kanalın kilidi başarıyla açıldı.**", color: Math.floor(Math.random() * (0xFFFFFF + 1)), timestamp: new Date()}});
+    msg.channel.updateOverwrite(msg.guild.roles.everyone, {
+      SEND_MESSAGES: null
+    });
+    locked[msg.channel.id].lock = false;
+  };
+};
+
+module.exports.help = {
+  name: "kanalkilit",
+  alias: ["kk"]
+};
