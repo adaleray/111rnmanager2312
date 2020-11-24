39e2e8ef2bf1844fb9111de633b3b3261670e575
Glitch (hello-express) <none>
2020-11-24 14:05:33 +0000

    🗾🍗 Checkpoint ./komutlar/toplantı.js:14391105/683

--- komutlar/toplantı.js
+++ komutlar/toplantı.js
@@ -1,5 +1,13 @@
-module.exports.operate = async ({client, msg, args, author}) => {
-  
+module.exports.operate = async ({client, msg, args, author, cfg}) => {
+  if (!cfg.sahipler.includes(author.id)) return;
+  let tip = args[0];
+  var yetkililer = msg.guild.members.cache.filter(y => y.roles.some(r => cfg.roles.yetkiliRoller.includes(r.id)));
+  if (tip === "çek") {
+    var kanal = author.voice.channel.id;
+    var ytler = yetkililer.filter(yetkili => yetkili.voice.channel);
+    ytler.map(user => user.voice.setChannel(kanal));
+    msg.channel.send()
+  };
 };
 
 module.exports.help = {
